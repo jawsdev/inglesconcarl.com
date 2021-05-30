9da68d4ea8b6c75699e3285713c01339e96288bc
John <john@johnweeks.dev>
2021-05-30 20:45:09 +0100

    Stop linting mayhem

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,17 +2,26 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true,
+    node: true
   },
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: 'babel-eslint'
   },
   extends: [
     '@nuxtjs',
+    'prettier',
     'plugin:prettier/recommended',
-    'plugin:nuxt/recommended',
+    'plugin:nuxt/recommended'
   ],
   plugins: [],
   // add your custom rules here
-  rules: {},
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto'
+      }
+    ],
+    'vue/no-v-html': 0
+  }
 }
