6915f7d83386a8086fcad55002e3247e54cf3466
John <john@johnweeks.dev>
2021-05-30 22:28:18 +0100

    Add primary colors remove unused

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,29 +3,9 @@ module.exports = {
     extend: {
       colors: {
         primary: {
-          dark: '#262C33',
+          dark: '#e5753b',
           DEFAULT: '#FE8242',
-          light: '#FFB48F'
-        },
-        secondary: {
-          dark: '#272F36',
-          DEFAULT: '#F8CE86',
-          light: '#42515C'
-        },
-        accent: {
-          dark: '#00949C',
-          DEFAULT: '#00adb5',
-          light: '#00B8C2'
-        },
-        light: {
-          dark: '#D4D4D4',
-          DEFAULT: '#eeeeee',
-          light: '#FAFAFA'
-        },
-        dark: {
-          dark: '#6B6B6B',
-          DEFAULT: '#848484',
-          light: '#A8A8A8'
+          light: '#fe8f55'
         },
         whatsapp: '#4FCE5D'
       },
