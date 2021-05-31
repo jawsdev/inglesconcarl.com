b0f1534770d7729efb0b625fab22e12362ba2860
John <john@johnweeks.dev>
2021-05-31 21:22:24 +0100

    remove console log

--- middleware/language.js
+++ middleware/language.js
@@ -1,7 +1,6 @@
 export default function ({ route, store }) {
   if (route.params.language) {
     const currentLanguage = route.params.language
-    console.log('currentLanguage', currentLanguage)
     store.dispatch('putLanguage', currentLanguage)
   }
 }
